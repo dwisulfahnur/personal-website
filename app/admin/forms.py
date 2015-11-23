99dbf7db723941869a2d4061fa13832fdb26b5da
dwisulfahnur <dwisulfahnur@gmail.com>
2015-11-23 19:14:03 +0800

    Add validate

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -4,12 +4,21 @@ from app.core.models import Blog
 
 
 class CreatePostForm(Form):
-    title  = StringField('Title', [validators.InputRequired(message='Title can not empty')])
-    author = StringField('Your Name', [validators.InputRequired(message='Name can not empty')])
-    content = TextAreaField('Text', [validators.InputRequired(message='Content can not empty')])
+    title  = StringField('Title', [validators.InputRequired(message='Please enter Post Title')])
+    author = StringField('Your Name', [validators.InputRequired(message='Please enter Your Name')])
+    content = TextAreaField('Text', [validators.InputRequired(message='Please enter Post Content')])
     category = StringField('Your Post Category',
-        [validators.InputRequired('Category of your Post can not empty'),
+        [validators.InputRequired('Please enter Post Category'),
          validators.Regexp(message='Category must have only uppercase letters', regex=r'^[A-Z\d ]+$')
         ])
 
-    
+    def validate(self):
+        rv = Form.validate(self)
+        if not rv:
+            return False
+
+        blog = Blog.query.filter_by(title = self.title.data).first()
+        if blog:
+            self.title.error = "Title already exist"
+            return False
+        return True
