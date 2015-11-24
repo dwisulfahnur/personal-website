f8a6a37d80a789532f5bab1f940bb99c1c3657e6
dwisulfahnur <dwisulfahnur@gmail.com>
2015-11-24 16:18:37 +0800

    Change database config

--- config/base.py
+++ config/base.py
@@ -17,8 +17,9 @@ SECURITY_TRACKABLE = True
 SECURITY_PASSWORD_HASH = 'bcrypt'
 SECURITY_PASSWORD_SALT = 'changetowhateveryouwant'
 CSRF_ENABLED = True
+WTF_CSRF_ENABLED = True
 CSRF_SESSION_KEY = 'changetowhateveryouwant'
 
 # DATABASE CONFIG
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(APP_DIR, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@localhost/personal_website'
